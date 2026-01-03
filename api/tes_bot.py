b18abe58d8fa3f32de15ec9124765b0b46bbdf0d
Timurhaip <timur2010rumit0102@gmail.com>
2026-01-03 10:00:14 +0300

    Add files via upload

---
+++ api/tes_bot.py
@@ -0,0 +1,39 @@
+import asyncio
+import logging
+import os
+from dotenv import load_dotenv
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
+
+# Объект бота
+bot = Bot("8081364002:AAEkPw6n7yORNM5xOFK-dzchaHnv_el2GDA")
+# Диспетчер
+dp = Dispatcher()
+
+# Хэндлер на команду /start
+@dp.message(Command("start"))   
+async def cmd_inline_url(message: types.Message, bot: Bot):
+    builder = InlineKeyboardBuilder()
+    builder.row(types.InlineKeyboardButton(
+        text="Open it", url="https://t.me/justtesseract_bot/ownerlink")
+    )
+    builder.row(types.InlineKeyboardButton(
+        text="Channel", url="https://t.me/justurlnomore")
+    )    
+    await message.answer(
+            'Hello, welcome to Tesseract',
+            reply_markup=builder.as_markup(),
+        )    
+
+# Запуск процесса поллинга новых апдейтов
+async def main():
+    # Удаляем вебхук и пропускаем накопившиеся входящие сообщения
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
